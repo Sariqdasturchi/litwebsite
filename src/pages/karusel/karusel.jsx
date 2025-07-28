25e42232b3385af488cb8239229bb173c027dad8
Shoxrux <fayzullayevshoxrux00@gmail.com>
2025-07-28 12:28:34 +0500

    tugadi

--- src/pages/karusel/karusel.jsx
+++ src/pages/karusel/karusel.jsx
@@ -6,11 +6,11 @@ import 'swiper/css/navigation';
 import './karusel.css';
 import { Navigation } from 'swiper/modules';
 
-import video1 from '../../assets/1.mp4';
-import video2 from '../../assets/2.mp4';
-import video3 from '../../assets/3.mp4';
-import video4 from '../../assets/4.mp4';
-import video5 from '../../assets/5.mp4';
+import video1 from '../../../public/videos/1.mp4';
+import video2 from '../../../public/videos/2.mp4';
+import video3 from '../../../public/videos/3.mp4';
+import video4 from '../../../public/videos/4.mp4';
+import video5 from '../../../public/videos/5.mp4';
 
 export default function Karusel() {
     const data = [
